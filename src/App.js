78e31eded2456c0198b7fffb7249f851b5320249
AdaN6 <a_disney_da@hotmail.com>
2022-11-29 17:32:55 +0100

    tried to fix the error of route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <div>
-          <Route index />
-          <Route path="pokemon/:id" />
-          <Route path="pokemon/:id/:info" />
-        </div>
-      </Router>
+        <Routes>
+          <div>
+            <Route index />
+            <Route path="pokemon/:id" />
+            <Route path="pokemon/:id/:info" />
+          </div>
+        </Routes>
     </>
   );
 }
