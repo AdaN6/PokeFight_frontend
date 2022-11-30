23fccaf5e9e531e573beb363b86975b92ae8b05a
AdaN6 <a_disney_da@hotmail.com>
2022-11-30 20:08:25 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home.js";
+import axios from "axios";
 
 function App() {
+//only for testing
+const pokeApiUrl = "https://lalala.cyclic.app/pokemon";
 
+const pokemonApi2 = async () => {
+  const result = await axios.get(pokeApiUrl).then((data) => data);
+console.log(result)
+  return result;
+};
 
+pokemonApi2()
 
   return (
     <>
         <Routes>
-            <Route index elemen={<Home />} />
+            <Route index element={<Home />} />
             <Route path="pokemon/:id" />
             <Route path="pokemon/:id/:info" />
         </Routes>
+        
     </>
   );
 }
