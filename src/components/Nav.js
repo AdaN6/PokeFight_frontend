c8d2c6d96ff7e3a27244a730c808d54dd322128d
Quantumminded <lukkino31@gmail.com>
2022-12-01 18:08:28 +0100

    adding card in home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,12 +7,12 @@ const Nav = () => {
     <div class="container flex flex-wrap items-center justify-between mx-auto">
     <a href="https://flowbite.com/" class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Poke App</span>
     </a>
     <div class="flex md:order-2">
         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-        <span class="sr-only">Search</span>
+        <span class="sr-only">Search your Pokemon</span>
         </button>
         <div class="relative hidden md:block">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+// /pokemon/:id/:info -> Super detailed view with only the names or the types and or the bases
 
 const PokemonInfo = () => {
   return (
