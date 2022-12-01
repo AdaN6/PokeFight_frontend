3db93c9f5480338fe97c0c11c54dd6bba92c2796
AdaN6 <a_disney_da@hotmail.com>
2022-12-01 12:19:03 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
 import { Routes, Route } from "react-router-dom";
+import {useState, useEffect} from "react"; 
 import Home from "./components/home.js";
-import axios from "axios";
+import { pokemonApi1 } from "./services/api1.js";
 
 function App() {
+  const [data, setData] = useState({});
 
-// only for testing
-const pokeApiUrl = "https://lalala.cyclic.app/pokemon";
-const pokemonApi1 = async () => {
-  try {
-    const result = await axios(pokeApiUrl).then((data) => data);
-    if (!result)
-      throw new Error(
-        `Fail to get datas with a status of ${result.status}`
-      );
-    console.log(result);
-    return result;
-  } catch (error) {
-    console.warn(error);
-  }
-};
 
-pokemonApi1();
+  useEffect(() => {
+    pokemonApi1().then((result) => setData(result));
+  }, []);
+
+  const testId = data.data?[0].id
+
+  // console.log(data);
+  console.log(testId);
+
+
+  //data.data[0].id
+
+  // only for testing
+  // const pokeApiUrl = "https://lalala.cyclic.app/pokemon";
+  // const pokemonApi1 = async () => {
+  //   try {
+  //     const result = await axios(pokeApiUrl).then((data) => data);
+  //     if (!result)
+  //       throw new Error(
+  //         `Fail to get datas with a status of ${result.status}`
+  //       );
+  //     console.log(result);
+  //     return result;
+  //   } catch (error) {
+  //     console.warn(error);
+  //   }
+  // };
+
+  // pokemonApi1();
+  // console.log(
+  //   `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonApi1.data[1].id}.png`
+  // );
+  // const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonApi1.id}.png`;
 
   return (
     <>
-        <Routes>
-            <Route index element={<Home />} />
-            <Route path="pokemon/:id" />
-            <Route path="pokemon/:id/:info" />
-        </Routes>
-        
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="pokemon/:id" />
+        <Route path="pokemon/:id/:info" />
+      </Routes>
     </>
   );
 
-  function newFunction() {
-    ;
-  }
+  function newFunction() {}
 }
 
 export default App;
