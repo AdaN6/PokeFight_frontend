0d6c8791020be10e092d0168baa5f62bfdc65a7a
valentina vittoria <valeviit@icloud.com>
2022-12-09 15:33:54 +0100

    create a play component finish the pokemon info

--- src/components/Arena.js
+++ src/components/Arena.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useEffect, useState } from "react";
 import { pokemonApi1 } from '../services/api1';
-
+import  Play  from "./Play";
 
 const Arena = () => {
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
+  const [startGame, setStartGame]=useState(false);
 
   useEffect(() => {
     const getPokemons = async () => {
-      await pokemonApi1().then(data => setData(prev => data));
+      await pokemonApi1().then(data => setData(prev => data ));
       setLoading(prev => !prev);
     };
     getPokemons();
@@ -18,14 +19,14 @@ const Arena = () => {
 
 
   if (data) {
-    console.log(data);
+    //giconsole.log( ...data);
   }
   
 //console.log(randomId)
 
 const pointsPlayer2 = 200 //we should pass the pokemon id
 
-//console.log(pointsPlayer2)
+
 
  function battle ( pointsPlayer2){
 let randomId = Math.floor(Math.random() * 300)
@@ -50,14 +51,17 @@ let pointsPlayer1 = randomId;
         loading ?
           <p>loading...</p>
           :
-          
           <div className='bg-yellow-500'>
             <div className="flex justify-center">
               <img src="http://2.bp.blogspot.com/-j_GR1Tq5tP0/VbY3ueWy4qI/AAAAAAAAIvE/wCjca8TaU6g/s1600/Logo%2BPokemon.png" className="items-center w-auto h-60 " alt="Flowbite Logo" />
             </div>
             <div className='text-center text-8xl'>
               <h1>Battle Arena</h1>
-              <button onClick={()=>battle(pointsPlayer2)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Start</button>
+              <button onClick={()=>setStartGame(true)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Start</button>
+              {startGame ? 
+              <Play data={data}/>
+              :
+              null}
             </div>
           </div>
       }
