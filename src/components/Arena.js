70efa6da34fc2618b562a5576091fc2019ceeaa4
AdaN6 <a_disney_da@hotmail.com>
2023-01-30 17:51:05 +0100

    added start button on battle arena

--- src/components/Arena.js
+++ src/components/Arena.js
@@ -38,78 +38,135 @@ const Arena2 = () => {
   console.log(pokemon[0].base)
 
   return (
-    <div className='bg-yellow-500 arena-container'>
-      <div className=' w-screen h-screen'>
-        <div className='flex text-center justify-center text-white text-8xl'>
-          <img src="http://2.bp.blogspot.com/-j_GR1Tq5tP0/VbY3ueWy4qI/AAAAAAAAIvE/wCjca8TaU6g/s1600/Logo%2BPokemon.png" className="items-center w-auto h-48 " alt="Flowbite Logo" />
+    <div className="bg-yellow-500 arena-container">
+      <div className=" w-screen pb-8">
+        <div className="flex text-center justify-center text-white text-8xl">
+          <img
+            src="http://2.bp.blogspot.com/-j_GR1Tq5tP0/VbY3ueWy4qI/AAAAAAAAIvE/wCjca8TaU6g/s1600/Logo%2BPokemon.png"
+            className="items-center w-auto h-44 "
+            alt="Flowbite Logo"
+          />
         </div>
-        <div className='flex justify-center gap-20'>
+        <div className="flex flex-col items-center pb-8">
+          <h5 class="inline-flex items-center px-8 py-4 text-m font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+              Start
+          </h5>
+        </div>
+        <div className="flex justify-center gap-20">
           {/* 1 pokemon card */}
           <div className="card">
-            <div className="pkmn__container" >
+            <div className="pkmn__container">
               <div className="pkmn__picture">
-                <div className="pkmn__cp">#
-                  <span>{pokemon[2].id}</span>
+                <div className="pkmn__cp">
+                  #<span>{pokemon[2].id}</span>
                 </div>
-                <img className="pkmn__png object-fill h-28" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon[2].id}.svg`} alt={pokemon[2].name.english} />
-                <div className="pkmn__exp-bar"></div>				
+                <img
+                  className="pkmn__png object-fill h-28"
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon[2].id}.svg`}
+                  alt={pokemon[2].name.english}
+                />
+                <div className="pkmn__exp-bar"></div>
               </div>
               <div className="pkmn__info">
-                  <div className="pkmn__name">{pokemon[2].name.english}
-                    <p className="size-medium"> Type: {pokemon[2].type[0]} {pokemon[2].type[1]}</p>
+                <div className="pkmn__name">
+                  {pokemon[2].name.english}
+                  <p className="size-medium">
+                    {" "}
+                    Type: {pokemon[2].type[0]} {pokemon[2].type[1]}
+                  </p>
+                </div>
+                <div className="pkmn__data">
+                  <div className="pkmn__type">
+                    {pokemon[2].base.Attack}
+                    <p className="text--small">Atack</p>
                   </div>
-                  <div className="pkmn__data">
-                    <div className="pkmn__type">{pokemon[2].base.Attack}<p className="text--small">Atack</p></div>
-                    <div className="pkmn__weight">{pokemon[2].base.Defense}<p className="text--small">Defense</p></div>
-                    <div className="pkmn__type">{pokemon[2].base.Speed}<p className="text--small">Speed</p></div>
-                    <div className="pkmn__height">{pokemon[2].base.HP}<p className="text--small">HP</p></div>
+                  <div className="pkmn__weight">
+                    {pokemon[2].base.Defense}
+                    <p className="text--small">Defense</p>
                   </div>
-                  <div className="pkmn__move space">
-                    <div>Sp Attack<p className="move--mtop">Sp Attack</p></div>
-                    <div>{pokemon[2].base['Sp. Attack']}</div>
+                  <div className="pkmn__type">
+                    {pokemon[2].base.Speed}
+                    <p className="text--small">Speed</p>
                   </div>
-                  <div className="pkmn__move">
-                    <div className=" move__special">Sp Defense<p className="move--mtop">Sp Defense</p></div>
-                    <div className="">{pokemon[2].base['Sp. Defense']}</div>
+                  <div className="pkmn__height">
+                    {pokemon[2].base.HP}
+                    <p className="text--small">HP</p>
                   </div>
                 </div>
+                <div className="pkmn__move space">
+                  <div>
+                    Sp Attack<p className="move--mtop">Sp Attack</p>
+                  </div>
+                  <div>{pokemon[2].base["Sp. Attack"]}</div>
+                </div>
+                <div className="pkmn__move">
+                  <div className=" move__special">
+                    Sp Defense<p className="move--mtop">Sp Defense</p>
+                  </div>
+                  <div className="">{pokemon[2].base["Sp. Defense"]}</div>
+                </div>
+              </div>
             </div>
           </div>
           {/* 2 pokemon card */}
           <div className="card">
-            <div className="pkmn__container" >
+            <div className="pkmn__container">
               <div className="pkmn__picture">
-                <div className="pkmn__cp">#
-                  <span>{pokemon[201].id}</span>
+                <div className="pkmn__cp">
+                  #<span>{pokemon[201].id}</span>
                 </div>
-                <img className="pkmn__png object-fill h-28" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon[201].id}.svg`} alt={pokemon[201].name.english} />
-                <div className="pkmn__exp-bar"></div>				
+                <img
+                  className="pkmn__png object-fill h-28"
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon[201].id}.svg`}
+                  alt={pokemon[201].name.english}
+                />
+                <div className="pkmn__exp-bar"></div>
               </div>
               <div className="pkmn__info">
-                  <div className="pkmn__name">{pokemon[201].name.english}
-                    <p className="size-medium"> Type: {pokemon[201].type[0]} {pokemon[201].type[1]}</p>
+                <div className="pkmn__name">
+                  {pokemon[201].name.english}
+                  <p className="size-medium">
+                    {" "}
+                    Type: {pokemon[201].type[0]} {pokemon[201].type[1]}
+                  </p>
+                </div>
+                <div className="pkmn__data">
+                  <div className="pkmn__type">
+                    {pokemon[201].base.Attack}
+                    <p className="text--small">Atack</p>
                   </div>
-                  <div className="pkmn__data">
-                    <div className="pkmn__type">{pokemon[201].base.Attack}<p className="text--small">Atack</p></div>
-                    <div className="pkmn__weight">{pokemon[201].base.Defense}<p className="text--small">Defense</p></div>
-                    <div className="pkmn__type">{pokemon[201].base.Speed}<p className="text--small">Speed</p></div>
-                    <div className="pkmn__height">{pokemon[201].base.HP}<p className="text--small">HP</p></div>
+                  <div className="pkmn__weight">
+                    {pokemon[201].base.Defense}
+                    <p className="text--small">Defense</p>
                   </div>
-                  <div className="pkmn__move space">
-                    <div>Sp Attack<p className="move--mtop">Sp Attack</p></div>
-                    <div>{pokemon[201].base['Sp. Attack']}</div>
+                  <div className="pkmn__type">
+                    {pokemon[201].base.Speed}
+                    <p className="text--small">Speed</p>
                   </div>
-                  <div className="pkmn__move">
-                    <div className=" move__special">Sp Defense<p className="move--mtop">Sp Defense</p></div>
-                    <div className="">{pokemon[201].base['Sp. Defense']}</div>
+                  <div className="pkmn__height">
+                    {pokemon[201].base.HP}
+                    <p className="text--small">HP</p>
                   </div>
                 </div>
+                <div className="pkmn__move space">
+                  <div>
+                    Sp Attack<p className="move--mtop">Sp Attack</p>
+                  </div>
+                  <div>{pokemon[201].base["Sp. Attack"]}</div>
+                </div>
+                <div className="pkmn__move">
+                  <div className=" move__special">
+                    Sp Defense<p className="move--mtop">Sp Defense</p>
+                  </div>
+                  <div className="">{pokemon[201].base["Sp. Defense"]}</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
   {
     loading ?
       <p>loading...</p>
